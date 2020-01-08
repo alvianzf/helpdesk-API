51583c013dc61af90e3cda38a0f5a745b4c8fbf6
Ari Putra <ariteknologi@gmail.com>
2020-01-09 00:10:12 +0700

    add cors package

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express'),
     app = express(),
     router = require('./routes'),
     jwt = require('jsonwebtoken'),
-    mongoose = require('mongoose')
+    mongoose = require('mongoose'),
+    cors = require('cors');
 
 console.log('Waiting connection from mongodb')
 mongoose.set('useCreateIndex', true)
@@ -16,6 +17,7 @@ mongoose.connect(process.env.DB_URL, {
 }).then( (db) => {
     console.log('Server connected to mongodb.......')
     app.set('secretKey', 'nodeRestApi')
+    app.use(cors())
     app.use(express.urlencoded({ extended: false }))
     app.use(express.json());
     app.use('/api', router)
