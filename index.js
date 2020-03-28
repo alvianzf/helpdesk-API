95d2112ed7ddf131633269853f22597f83e7b84c
Ari Putra <ariteknologi@gmail.com>
2020-03-28 18:27:15 +0700

    testing cors

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ mongoose.connect(process.env.DB_URL, {
     console.log('Server connected to mongodb.......')
     app.set('secretKey', 'nodeRestApi')
     app.use(cors())
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
     app.use(express.urlencoded({ extended: false }))
     app.use(express.json());
     app.use(express.static('public/images'))
