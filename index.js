a27ba62f74421d08638916dae2c49e6d43c73c42
Ari Putra <ariteknologi@gmail.com>
2020-03-28 18:45:59 +0700

    fix cors

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ mongoose.connect(process.env.DB_URL, {
     useUnifiedTopology : true,
     useFindAndModify: false
 }).then( (db) => {
+    let corsOptions = {
+        origin : "*",
+        methods : "*",
+        allowedHeaders : "*",
+        exposedHeaders : '*',
+        credentials : true
+    }
     console.log('Server connected to mongodb.......')
     app.set('secretKey', 'nodeRestApi')
-    app.use(cors())
-    app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', '*');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        next();
-    });
+    app.use(cors(corsOptions))
     app.use(express.urlencoded({ extended: false }))
     app.use(express.json());
     app.use(express.static('public/images'))
